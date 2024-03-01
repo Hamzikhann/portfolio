a9688b5280673a1f42d44a46029108b7a2b0a5dd
hamza-qasim <hamzaqasim.c@gmail.com>
2024-03-01 17:35:28 +0500

    counter added

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,19 +1,28 @@
 import React from "react";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+import { motion } from "framer-motion";
 
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
-  return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<h2 className={`${styles.sectionHeadText} text-center mb-3`}>Our Tech </h2>
+			</motion.div>
+
+			<div className="flex flex-row flex-wrap justify-center gap-10 ">
+				{technologies.map((technology) => (
+					<div className="w-28 h-28" key={technology.name}>
+						<BallCanvas icon={technology.icon} />
+					</div>
+				))}
+			</div>
+		</>
+	);
 };
 
 export default SectionWrapper(Tech, "");
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from "./canvas";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
@@ -8,20 +8,21 @@ import Works from "./Works";
 import Feedbacks from "./Feedbacks";
 import Contact from "./Contact";
 import CanvasLoader from "./Loader";
-
+import Count from "./Count";
 
 export {
-  Hero,
-  Navbar,
-  About,
-  Tech,
-  Experience,
-  Works,
-  Feedbacks,
-  Contact,
-  CanvasLoader,
-  EarthCanvas, 
-  BallCanvas, 
-  ComputersCanvas, 
-  StarsCanvas
+	Count,
+	Hero,
+	Navbar,
+	About,
+	Tech,
+	Experience,
+	Works,
+	Feedbacks,
+	Contact,
+	CanvasLoader,
+	EarthCanvas,
+	BallCanvas,
+	ComputersCanvas,
+	StarsCanvas
 };
