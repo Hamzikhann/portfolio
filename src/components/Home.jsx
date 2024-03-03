7eabf2a1abacecc12604fcf5c254a2c6139f42f7
hamza qasim <hamzaqasim.c@gmail.com>
2024-03-03 23:05:06 +0500

    some changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,6 +20,7 @@ const Home = () => {
 {/* //  <div className="relative z-0 bg-primary"> */}
 				<div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
 					{/* <Navbar /> */}
+hi
 					<Hero />
 				</div>
 				<About />
