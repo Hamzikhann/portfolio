f7708b51b28d3fc2c2ebf84c304e903d6239a659
hamza-qasim <hamzaqasim.c@gmail.com>
2024-10-28 23:06:48 +0500

    logo updated

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import envCompatible from "vite-plugin-env-compatible";
-
+import replace from 'vite-plugin-replace';
 // https://vitejs.dev/config/
 export default defineConfig({
 	envPrefix: "REACT_APP_",
-	plugins: [react(), envCompatible()]
+	plugins: [react(), envCompatible(),
+		replace({
+			values: {
+				'"use client";': '', // Replaces "use client"; with an empty string
+			},
+			include: ["node_modules/flowbite-react/**/*.js"], // Only applies to flowbite-react module files
+		})
+		
+	]
+	
 });
